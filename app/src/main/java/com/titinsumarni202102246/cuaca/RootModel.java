adad739bdd2730842b3b73d494d993ff37f6f944
titinsumarni19 <92568394+titinsumarni19@users.noreply.github.com>
2023-06-19 09:45:35 +0700

    Initial commit

---
+++ app/src/main/java/com/titinsumarni202102246/cuaca/RootModel.java
@@ -0,0 +1,21 @@
+package com.titinsumarni202102246.cuaca;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class RootModel {
+    @SerializedName("list")
+    private List<ListModel> listModelList;
+
+    public RootModel() {
+    }
+
+    public List<ListModel> getListModelList() {
+        return listModelList;
+    }
+
+    public void setListModelList(List<ListModel> listModelList) {
+        this.listModelList = listModelList;
+    }
+}
